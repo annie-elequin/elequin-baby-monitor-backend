df46cce587e2fa6913b00108f2fab7cc52e24409
Annie Elequin <annie@elequin.io>
2019-05-14 19:44:31 -0500

    added email functionality

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const nodemailer = require('nodemailer');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.get('/send-email', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'elequinbabymonitor@gmail.com',
+      pass: 'elequin.baby.monitor.2019'
+    }
+  });
+
+  const mailOptions = {
+    from: 'elequinbabymonitor@gmail.com',
+    // to: 'annie@elequin.io',
+    to: 'bbrooklynrider96@gmail.com',
+    subject: 'BABY MONITOR',
+    text: 'Sounds like the baby might be awake.'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('Email not sent.');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email sent!');
+    }
+  });
+});
 
 app.listen(port, () =>
-  console.log(`Example app listening at: https://localhost:${port}`)
+  console.log(`Example app listening at: http://localhost:${port}`)
 );
